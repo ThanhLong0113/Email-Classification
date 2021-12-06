029844a3deb96dec1142664a7f1fc2a910ecc90f
callmeweed <you@example.com>
2021-12-06 17:31:19 +0700

    Test với bộ dữ liệu 250 email

---
+++ bayes.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import numpy as np
+from gensim.parsing.preprocessing import strip_non_alphanum, strip_multiple_whitespaces,preprocess_string, split_alphanum, strip_short, strip_numeric
+import re
+
+
+def raw_text_preprocess(raw):
+    raw = re.sub(r"http\S+", "", raw)  #bỏ đường dẫn
+    raw = strip_non_alphanum(raw).lower().strip()   #loại bỏ kí tự lạ, chuẩn hóa
+    raw = split_alphanum(raw)   #tách các từ vô nghĩa
+    raw = strip_short(raw, minsize=2)   # loại bỏ kí tự đứng 1 mình
+    raw = strip_numeric(raw)   # Loại bỏ số 
+
+    return raw
+
+xls_file = pd.ExcelFile('./Book1.xlsx')
+dfs = xls_file.parse('Sheet1')
+
+document = []
+label = []
+
+for d in dfs.Document:
+    d = raw_text_preprocess(d)
+    document.append(d)
+for l in dfs.Label:
+    label.append(l)
+
+document = [raw_text_preprocess(d) for d in document]
+
+#xay dung bag of words
+
+set_words = []
+
+for doc in document:
+    words = doc.split(' ')
+    set_words += words
+    set(set_words)
+
+print("Kich thuoc BAG WORDS:" , len(set_words))
+
+
+
+#Chuyển mail sang vector
+vectors = []
+
+for doc in document:
+    vector = np.zeros(len(set_words))
+    for i, word in enumerate(set_words):
+        if word in doc:
+            vector[i] = 1
+    vectors.append(vector)
+
+print("So vector, so chieu: ",np.shape(vectors))
+
+def smoothing(a, b):
+    return float((a+1)/(b+1))
+
+# P(spam), P(nonspam)
+spam = 0
+non_spam = 0
+for l in label:
+    if l == 1:
+        spam += 1
+    else:
+        non_spam += 1
+print("So thu Spam, Nonspam: ",spam, non_spam)
+
+spam_coef = smoothing(spam, (spam+non_spam))
+non_spam_coef = smoothing(non_spam, (spam+non_spam))
+
+bayes_matrix = np.zeros((len(set_words), 4)) #app/spam, app/nonspam, nonapp/spam, nonapp/nonspam
+
+for i, word in enumerate(set_words):
+    app_spam = 0
+    app_nonspam = 0
+    nonapp_spam = 0
+    nonapp_nonspam = 0
+    for k, v in enumerate(vectors):
+        if v[i] == 1:
+            if label[k] == 1:
+                app_spam += 1
+            else:
+                app_nonspam += 1
+        else:
+            if label[k] == 1:
+                nonapp_spam += 1
+            else:
+                nonapp_nonspam += 1
+                
+    bayes_matrix[i][0] = smoothing(app_spam, spam)
+    bayes_matrix[i][1] = smoothing(app_nonspam, non_spam)
+    bayes_matrix[i][2] = smoothing(nonapp_spam, spam)
+    bayes_matrix[i][3] = smoothing(nonapp_nonspam, non_spam)
+
+def compare(predict_spam, predict_non_spam, log):
+    while (log[0] > log[1]):
+        predict_spam /= 10
+        log[0] -=1
+        if predict_spam > predict_non_spam:
+            return True
+        
+    while(log[1] > log[0]):
+        predict_non_spam /= 10
+        log[1] -= 1
+        if predict_non_spam > predict_spam:
+            return False
+        
+    if predict_spam > predict_non_spam:
+        return True
+    return False
+
+def predict(mail):
+    mail = raw_text_preprocess(mail)
+    
+    vector = np.zeros(len(set_words))
+    for i, word in enumerate(set_words):
+        if word in mail:
+            vector[i] = 1
+    log = np.zeros(2)
+
+    predict_spam = spam_coef
+    predict_non_spam = non_spam_coef
+
+    for i, v in enumerate(vector):
+        if v == 0:
+            predict_spam *= bayes_matrix[i][2]
+            predict_non_spam *= bayes_matrix[i][3]
+        else:
+            predict_spam *= bayes_matrix[i][0]
+            predict_non_spam *= bayes_matrix[i][1]
+
+        if predict_spam < 1e-10:
+            predict_spam *= 1000
+            log[0] += 1
+
+        if predict_non_spam < 1e-10:
+            predict_non_spam *= 1000
+            log[1] +=1
+            
+    if compare(predict_spam, predict_non_spam, log):
+        return 1
+    return 0
+
+
+print("TESTTTTT")
+test = pd.ExcelFile('./Book2.xlsx')
+df_test = test.parse('Sheet1')
+document_test = []
+label_test = []
+
+for d in df_test.Document:
+    document_test.append(d)
+    print(predict(d))
+for l in df_test.Label:
+    label_test.append(l)    
+
+from sklearn.metrics import accuracy_score
+
+pred = [predict(d) for d in document_test]
+print(accuracy_score(label_test, pred))
